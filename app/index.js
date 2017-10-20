a45402b4be011cb31c92cca894c4bfd9fea61d81
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-20 11:39:18 +0200

    init middleware

--- app/index.js
+++ app/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux'
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 import Home from 'components/home';
 import Vod from 'components/vod';
 import reducers from 'reducers';
+import {timerMiddleware} from 'middlewares';
 import createHistory from 'history/createBrowserHistory'
-import {Route} from 'react-router'
-import {ConnectedRouter, routerReducer, routerMiddleware, push} from 'react-router-redux'
+import { Route } from 'react-router'
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(timerMiddleware));
 const history = createHistory();
-const middleware = routerMiddleware(history);
+//const middleware = routerMiddleware(history);
 
-store.dispatch({type: 'RANDOM'});
+store.dispatch({ type: 'RANDOM' });
+store.dispatch({ type: 'RAIL' });
 
 render(
   <Provider store={store}>
-  <ConnectedRouter history={history}>
-    <div>
-      <Route exact={true} path='/' component={Home}/>
-      <Route path='/vod' component={Vod}/>
-    </div>
-  </ConnectedRouter>
-</Provider>, document.getElementById('container'));
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact={true} path='/' component={Home} />
+        <Route path='/vod' component={Vod} />
+      </div>
+    </ConnectedRouter>
+  </Provider>, document.getElementById('container'));
