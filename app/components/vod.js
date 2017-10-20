a9cf97d4ab2b2c0dbb3e332812a662c9d89f1682
8p4hxc2 <8p4hxc2@gmail.com>
2017-10-20 11:40:20 +0200

    style to vod

--- app/components/vod.js
+++ app/components/vod.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+import "styles/vod";
 
 class Vod extends Component {
   render() {
     return (
       <div>
         <h1 className='zzzz'>WHUT?VOODDDD{this.props.random}</h1>
-        <Link to={'/'}>hehe</Link>
+        <Link to={'/'}>Home</Link>
+        <div className='menu'>
+          {this.props.rail.map((x, i) => <div className={i === 0
+            ? 'omg'
+            : 'haha'} key={i}>{this.props.rail[i]}</div>)}
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  return {random: state.random};
+  return {rail: state.rail};
 }
 
 export default connect(mapStateToProps)(Vod);
